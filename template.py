e537d76855f3dd567b3b2954f43a145697319080
York-yanyu <gyf3344@163.com>
2020-09-17 17:10:15 +0800

    add a template (unfinished)

---
+++ template.py
@@ -0,0 +1,20 @@
+#引入包
+import tensorflow as tf
+from tensorflow import keras
+#数据处理
+
+
+
+#神经网络设计
+number1 = 1
+number2 = 2
+number3 = 3
+model = tf.keras.Sequential()
+model.add(keras.layers.Dense(number1, activation=''))
+model.add(keras.layers.Dense(number2, activation=''))
+model.add(keras.layers.Dense(number3, activation=''))
+
+#神经网络训练及可视化展示
+
+
+#其他（如何导出，如何使用等等）
