afbd63d8ba99d7d5f54a9b4ca217080d607fafc5
York-yanyu <gyf3344@163.com>
2020-09-17 17:18:24 +0800

    ddddd

--- 1-MNIST.py
+++ 1-MNIST.py
@@ -16,5 +16,5 @@ model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test,  y_test, verbose=2)
+model.evaluate(x_test, y_test, verbose=2)
 ##调试git1
--- template.py
+++ template.py
@@ -1,8 +1,9 @@
 #引入包
+#Keras是一个由Python编写的开源人工神经网络库，可以作为Tensorflow、Microsoft-CNTK和Theano的高阶应用程序接口，进行深度学习模型的设计、调试、评估、应用和可视化
 import tensorflow as tf
 from tensorflow import keras
 #数据处理
-
+testset = [1,2,3]
 
 
 #神经网络设计
@@ -15,6 +16,7 @@ model.add(keras.layers.Dense(number2, activation=''))
 model.add(keras.layers.Dense(number3, activation=''))
 
 #神经网络训练及可视化展示
-
+model.compile(optimizer=' ', loss=' ', metrics=[' '])
+model.fit(testset, epochs=10)
 
 #其他（如何导出，如何使用等等）
